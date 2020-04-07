cc9020782b97b3e1edcf10ff57ce62079f45b105
jadecci <vesaveronica@gmail.com>
2020-04-07 15:24:58 +0200

    Added comparisonfor nRMSD results too

--- unit_test/unit_test_compare.m
+++ unit_test/unit_test_compare.m
@@ -2,11 +2,11 @@ function unit_test_compare(file1, file2)
 % This function compares two CBPP performance files and returns 1 if their r_test variables are
 % the same
 %
-% Jianxiao Wu, last edited on 12-Sept-2019
+% Jianxiao Wu, last edited on 04-Apr-2020
 
-perf1 = load(file1, 'r_test');
-perf2 = load(file2, 'r_test');
-diff = sum(abs(perf1.r_test(:) - perf2.r_test(:)));
+perf1 = load(file1, 'r_test', 'nrmsd_test');
+perf2 = load(file2, 'r_test', 'nrmsd_test');
+diff = sum(abs(perf1.r_test(:) - perf2.r_test(:)) + abs(perf1.nrmsd_test(:) - perf2.nrmsd_test(:)));
 
 disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 if diff ~= 0
