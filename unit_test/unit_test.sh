01e2b6ce076fd7c08cb18b375b2c3adb253e422e
jadecci <vesaveronica@gmail.com>
2020-04-09 15:36:38 +0200

    Added back comparison for volumetric unit test

--- unit_test/unit_test.sh
+++ unit_test/unit_test.sh
@@ -124,10 +124,10 @@ wb_output=$output_dir/CBPP_perf/wbCBPP_SVR_standard_HCP_fix_wmcsf_AICHA_Pearson_
 wb_compare=$ROOT_DIR/unit_test/ground_truth/wbCBPP_SVR_standard_HCP_fix_wmcsf_AICHA_Pearson_fixSeed.mat
 pw_output=$output_dir/CBPP_perf/pwCBPP_SVR_standard_HCP_fix_wmcsf_AICHA_Pearson_fixSeed_parcel$parc_ind_mni.mat
 pw_compare=$ROOT_DIR/unit_test/ground_truth/pwCBPP_SVR_standard_HCP_fix_wmcsf_AICHA_Pearson_fixSeed_parcel$parc_ind_mni.mat
-#matlab -nodesktop -nosplash -r "addpath('$ROOT_DIR/unit_test'); \
-#                                unit_test_compare('$wb_output', '$wb_compare'); \
-#                                unit_test_compare('$pw_output', '$pw_compare'); \
-#                                exit"
+matlab -nodesktop -nosplash -r "addpath('$ROOT_DIR/unit_test'); \
+                                unit_test_compare('$wb_output', '$wb_compare'); \
+                                unit_test_compare('$pw_output', '$pw_compare'); \
+                                exit"
 
 # clean up
 rm $sublist_surf $sublist_mni
