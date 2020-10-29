c5d5a2506b8ff368df7bbafbb8c3c057642c338f
Wu Jianxiao <jianxiao.wu.veronica@gmail.com>
2020-10-30 00:47:49 +0100

    Change RR hyperparameter runing to be like EN

--- utilities/RR_one_fold.m
+++ utilities/RR_one_fold.m
@@ -1,5 +1,5 @@
-function [perf, weights, b, k] = RR_one_fold(x, y, conf, cv_ind, fold, seed, ks)
-% [perf, weights, b, k] = RR_one_fold(x, y, conf, cv_ind, fold, ks)
+function [perf, weights, b, k] = RR_one_fold(x, y, cv_ind, fold, ks)
+% [perf, weights, b, k] = RR_one_fold(x, y, cv_ind, fold, ks)
 %
 % This function runs Ridge Regression for one cross-validation fold. The relationship between features and targets is 
 % assumed to be y = x * weights + b.
@@ -28,17 +28,17 @@ function [perf, weights, b, k] = RR_one_fold(x, y, conf, cv_ind, fold, seed, ks)
 %       - b      :
 %                 Intercept value
 %
-% Jianxiao Wu, last edited on 21-Oct-2020
+% Jianxiao Wu, last edited on 30-Oct-2020
 
 % usage
-if nargin < 6
-    disp('Usage: [perf, weights, b, k] = RR_one_fold(x, y, conf, cv_ind, fold, ks)');
+if nargin < 4
+    disp('Usage: [perf, weights, b, k] = RR_one_fold(x, y, cv_ind, fold, ks)');
     return
 end
 
 % default k values
 % see sklearn.linear_model.RidgeCV for list of k/alpha values
-if nargin < 7
+if nargin < 5
     ks = 0:0.1:10;
 end
 
@@ -47,38 +47,23 @@ x_train = x(cv_ind ~= fold, :);
 y_train = y(cv_ind ~= fold);
 x_test = x(cv_ind == fold, :);
 y_test = y(cv_ind == fold);
-if numel(conf) ~= 0
-	conf_train = conf(cv_ind ~= fold, :);
-	conf_val = conf(cv_ind == fold, :);
-end
 
-% inner-loop cross-validation
-rng(seed);
-inner_ind = cvpartition(size(x_train, 1), 'KFold', 10); %10-fold CV
-r_k = zeros(length(ks), 1);
-for fold_inner = 1:10
-    x_train_inner = x_train(inner_ind.training(fold_inner), :);
-    y_train_inner = y_train(inner_ind.training(fold_inner));
-    x_val_inner = x_train(inner_ind.test(fold_inner), :);
-    y_val_inner = y_train(inner_ind.test(fold_inner));
+% inner-loop sets
+n_fold = max(cv_ind);
+if fold == n_fold; fold_inner = 1; else; fold_inner = fold + 1; end
+x_val_inner = x(cv_ind==fold_inner, :);
+y_val_inner = y(cv_ind==fold_inner);
+train_ind_inner = (cv_ind ~= fold) .* (cv_ind ~= fold_inner);
+x_train_inner = x(train_ind_inner==1, :);
+y_train_inner = y(train_ind_inner==1);
 
-    % regress out confounds if necessary
-    if numel(conf) ~= 0
-        conf_train_inner = conf_train(inner_ind.training(fold_inner), :);
-        conf_val_inner = conf_train(inner_ind.test(fold_inner), :);
-        [y_train_inner, reg_y] = regress_confounds_y(y_train_inner, conf_train_inner);
-        y_val_inner = regress_confounds_y(y_val_inner, conf_val_inner, reg_y);
-    end
+% find best k
+model = fitrlinear(x_train_inner, y_train_inner, 'Learner', 'leastsquares', 'Regularization', 'ridge', 'Lambda', ks);
+y_val_pred_inner = x_val_inner * model.Beta + model.Bias;
+r_val = corr(y_val_pred_inner, y_val_inner, 'type', 'Pearson', 'Rows', 'complete');
 
-    % test different k values
-    model = fitrlinear(x_train_inner, y_train_inner, 'Learner', 'leastsquares', 'Regularization', 'ridge', 'Lambda', ks);
-    y_val_pred_inner = x_val_inner * model.Beta + model.Bias;
-    r_curr = corr(y_val_pred_inner, y_val_inner, 'type', 'Pearson', 'Rows', 'complete');
-    r_k = r_k + r_curr;
-end
-
-% Best model
-[~, k_ind] = max(r_k);
+% best model
+[~, k_ind] = max(r_val);
 k = ks(k_ind);
 model = fitrlinear(x_train, y_train, 'Learner', 'leastsquares', 'Regularization', 'ridge', 'Lambda', k);
 weights = model.Beta;
