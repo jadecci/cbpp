359ceffa510a56906b5291783dfcea8d90324019
Wu Jianxiao <jianxiao.wu.veronica@gmail.com>
2021-11-24 15:08:23 +0100

    Restrict precision for unit test comparison

--- unit_test/unit_test_compare.m
+++ unit_test/unit_test_compare.m
@@ -6,11 +6,16 @@ function unit_test_compare(file1, file2)
 
 perf1 = load(file1, 'r_test', 'nrmsd_test');
 perf2 = load(file2, 'r_test', 'nrmsd_test');
-diff = sum(abs(perf1.r_test(:) - perf2.r_test(:)) + abs(perf1.nrmsd_test(:) - perf2.nrmsd_test(:)));
+perf1_r = round(perf1.r_test, 6);
+perf1_n = round(perf1.nrmsd_test, 6);
+perf2_r = round(perf2.r_test, 6);
+perf2_n = round(perf2.nrmsd_test, 6);
+diff = sum(abs(perf1_r(:) - perf2_r(:)) + abs(perf1_n(:) - perf2_n(:)));
 
 disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 if diff ~= 0
   disp('%%  The two results are different!  %%')
+  disp(['%%  Difference: ' num2str(diff) '  %%'])
 else
   disp('%%  The two results are identical.  %%')
 end
